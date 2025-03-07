bee1c700853e826f65d872f81a592b23111aa03d
Maria Antonya Fernandes da Silva <mariaantonyafernandes@gmail.com>
2025-03-07 09:37:36 -0300

    Update Variavel.java

--- secao2/Variavel.java
+++ secao2/Variavel.java
@@ -2,6 +2,9 @@ package secao2;
 
 public class Variavel {
     public static void main(String[] args) {
-        
+        // 1 - O que são variáveis?
+        // tipo -> nome -> atribuir um valor;
+        String nome = "Maria Antonya";
+        System.out.println(nome);
     }
 }
