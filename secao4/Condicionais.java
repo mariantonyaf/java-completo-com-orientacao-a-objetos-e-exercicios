c01cec688ab592b5f116db673a1c0138f97c1576
Maria Antonya Fernandes da Silva <mariaantonyafernandes@gmail.com>
2025-03-10 15:56:25 -0300

    Create Condicionais.java

---
+++ secao4/Condicionais.java
@@ -0,0 +1,20 @@
+package secao4;
+
+public class Condicionais {
+    public static void main(String[] args) {
+        // 1 - O que é boolean?
+        boolean isTrue = true;
+        boolean isFalse = false;
+
+        System.out.println(isTrue);
+        System.out.println(isFalse);
+        
+        // 2 - Operadores de comparação
+        int x = 10;
+
+        System.out.println(x == 10);
+        System.out.println(x == 9);
+        System.out.println(x != 9);
+        System.out.println(x != 10);
+    }
+}
