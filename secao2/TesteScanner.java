312afff6b83ae618da216607c47dffc8a4e96977
Maria Antonya Fernandes da Silva <mariaantonyafernandes@gmail.com>
2025-03-10 08:37:07 -0300

    Update TesteScanner.java

--- secao2/TesteScanner.java
+++ secao2/TesteScanner.java
@@ -4,12 +4,19 @@ import java.util.Scanner;
 
 public class TesteScanner {
     public static void main(String[] args) {
-        // 14 -Classe Scanner
+        // 1 - Classe Scanner
         // nextLine(): Lê uma linha inteira de texto;
         // nextInt(): Lê um valor inteiro;
         // nextDouble(): Lê um valor decimal (ponto flutuante);
         // next(): Lê uma única palavra;
+        Scanner scanner = new Scanner(System.in);
 
-        
+        // Mensagem para o usuario entender que precisa digitar
+        System.out.println("Digite seu nome: ");
+        // Resgata o valor do terminal
+        String nome = scanner.nextLine();
+
+        // Exibe o valor
+        System.out.println("Olá, " + nome + "!");
     }
 }
