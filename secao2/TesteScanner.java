ec802d0de62158d8f0965eea1b46965f0820e7b4
Maria Antonya Fernandes da Silva <mariaantonyafernandes@gmail.com>
2025-03-10 08:33:03 -0300

    Create TesteScanner.java

---
+++ secao2/TesteScanner.java
@@ -0,0 +1,15 @@
+package secao2;
+
+import java.util.Scanner;
+
+public class TesteScanner {
+    public static void main(String[] args) {
+        // 14 -Classe Scanner
+        // nextLine(): Lê uma linha inteira de texto;
+        // nextInt(): Lê um valor inteiro;
+        // nextDouble(): Lê um valor decimal (ponto flutuante);
+        // next(): Lê uma única palavra;
+
+        
+    }
+}
