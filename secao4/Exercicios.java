018755afc797c2eb8926fcef39d0c48619bca04e
Maria Antonya Fernandes da Silva <mariaantonyafernandes@gmail.com>
2025-03-11 18:03:10 -0300

    Create Exercicios.java

---
+++ secao4/Exercicios.java
@@ -0,0 +1,40 @@
+package secao4;
+
+import java.util.Scanner;
+
+public class Exercicios {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        /*
+        EXERCÍCIO 1:
+        Peça ao usuário para inserir o preço de um produto.
+        Se o preço for menor que 50, categorize como "Barato".
+        Se estiver entre 50 e 100, categorize como "Médio".
+        Se for maior que 100, categorize como "Caro".
+        Exiba a categoria correspondente.
+        */
+
+        System.out.println("Insira o preço do produto:");
+        double preco = scanner.nextDouble();
+
+        if (preco < 50) {
+            System.out.println("Barato.");
+        } else if (preco > 50 && preco < 100) {
+            System.out.println("Médio.");
+        } else {
+            System.out.println("Caro.");
+        }
+
+        scanner.close();
+    }
+
+    /*
+    EXERCÍCIO 2:
+    Peça ao usuário para inserir um nome de usuário e uma senha.
+    Verifique se o nome de usuário é "admin" e a senha é "1234".
+    Se ambos forem corretos, exiba "Acesso permitido".
+    Caso contrário, exiba "Acesso negado".
+    */
+
+    
+}
