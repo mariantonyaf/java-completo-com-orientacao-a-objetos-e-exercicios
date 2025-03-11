d65067891135f4888056317c2e994b586f49fab5
Maria Antonya Fernandes da Silva <mariaantonyafernandes@gmail.com>
2025-03-11 17:34:07 -0300

    Update OperadoresLogicos.java

--- secao4/OperadoresLogicos.java
+++ secao4/OperadoresLogicos.java
@@ -35,5 +35,34 @@ public class OperadoresLogicos {
         // 9 - NOT
         System.out.println(estaChovendo);
         System.out.println(!estaChovendo);
+
+        // 11 - Switch case e break
+        int diaDaSemana = 5;
+
+        switch (diaDaSemana) {
+            case 1: 
+            System.out.println("Domingo");
+                break;
+            case 2:
+            System.out.println("Segunda-feira");
+                break;
+            case 3:
+            System.out.println("Terça-feira");
+                break;
+            case 4:
+            System.out.println("Quarta-feira");
+                break;
+            case 5:
+            System.out.println("Quinta-feira");
+                break;
+            case 6:
+            System.out.println("Sexta-feira");
+                break;
+            case 7:
+            System.out.println("Sábado");
+                break;
+            default:
+                break;
+        }
     }
 }
