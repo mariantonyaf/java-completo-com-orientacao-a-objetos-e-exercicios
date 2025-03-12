428be5419027ff1f77232a744b586c19beb51927
Maria Antonya Fernandes da Silva <mariaantonyafernandes@gmail.com>
2025-03-12 10:52:25 -0300

    Create Funcoes2.java

---
+++ secao6/Funcoes2.java
@@ -0,0 +1,21 @@
+package secao6;
+
+public class Funcoes2 {
+    public static void main(String[] args) {
+        // 5 - Funções com condicionais
+        String r1 = verificarAcesso(22, true, false);
+
+        System.out.println(r1);
+    }
+
+    public static String verificarAcesso (int idade, 
+        boolean temCarteira,
+        boolean temHistoricoNegativo) {
+
+            if (idade >= 18 && temCarteira && !temHistoricoNegativo) {
+                return "Acesso permitido: todos os critérios atendidos!";
+            } else {
+                return "Acesso negado: critérios não atendidos!";
+            }
+    }
+}
