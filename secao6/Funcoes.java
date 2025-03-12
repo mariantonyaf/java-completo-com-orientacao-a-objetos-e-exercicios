690083da556a259b2809024ea0dd397789905319
Maria Antonya Fernandes da Silva <mariaantonyafernandes@gmail.com>
2025-03-12 10:25:36 -0300

    Update Funcoes.java

--- secao6/Funcoes.java
+++ secao6/Funcoes.java
@@ -2,8 +2,23 @@ package secao6;
 
 public class Funcoes {
     public static void main(String[] args) {
-        // Criando a primeira função
+        // 1 - Criando a primeira função
         saudacao();
+
+        // 2 - Parâmetros
+        soma(50, 5);
+        soma(10, 12);
+
+        saudar("Maria Antonya");
+
+        // 3 - return
+        dobrar(4);
+
+        int numero = 10;
+        int numeroDobrado = dobrar(numero);
+        System.out.println("Dobro: " + numeroDobrado);
+
+        System.out.println(dobrar(54));
     }
 
     /* NIVEL DE ACESSO, STATIC => não preciso instanciar classe para 
@@ -11,5 +26,18 @@ public class Funcoes {
     // NOME, OS PARENTESES (ARGS), BLOCO {}
     public static void saudacao () {
         System.out.println("Primeira função!");
+    }
+
+    public static void soma (int a, int b) {
+        int resultado = a + b;
+        System.out.println("Soma de A + B = " + resultado);
+    }
+
+    public static void saudar (String nome) {
+        System.out.println("Olá, " + nome + ", tudo bem?");
+    }
+
+    public static int dobrar (int n) {
+        return n * 2;
     }
 }
