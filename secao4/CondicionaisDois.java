911862df37ef4918de14f41382f4c4bca6d55f87
Maria Antonya Fernandes da Silva <mariaantonyafernandes@gmail.com>
2025-03-12 08:52:04 -0300

    Create CondicionaisDois.java

---
+++ secao4/CondicionaisDois.java
@@ -0,0 +1,16 @@
+package secao4;
+
+public class CondicionaisDois {
+    public static void main(String[] args) {
+        // 1 - Condicional ternária
+
+        int numero = 11;
+
+        String resultado = (numero % 2 == 0) ? "Par" : "Ímpar";
+
+        System.out.println(resultado);
+
+        /* CONDIÇÃO X > 5 ? EXPRESSÃO SE É TRUE "Ok" : EXPRESSÃO SE É
+        FALSE "Não ok" */
+    }
+}
