be3de823233a84535d5beba5057486d7a8ac2e93
Maria Antonya Fernandes da Silva <mariaantonyafernandes@gmail.com>
2025-03-12 13:07:49 -0300

    Update Funcoes2.java

--- secao6/Funcoes2.java
+++ secao6/Funcoes2.java
@@ -6,6 +6,11 @@ public class Funcoes2 {
         String r1 = verificarAcesso(22, true, true);
 
         System.out.println(r1);
+
+        // 6 - Funções com Switch
+        String diaSemana = obterDiaDaSemana(4);
+
+        System.out.println(diaSemana);
     }
 
     public static String verificarAcesso (int idade, 
@@ -19,5 +24,27 @@ public class Funcoes2 {
             } else {
                 return "Acesso negado: critérios não atendidos!";
             }
+    }
+
+    public static String obterDiaDaSemana (int dia) {
+        switch (dia) {
+            case 1:
+                return "Domingo";
+            case 2:
+                return "Segunda-feira";
+            case 3:
+                return "Terça-feira";
+            case 4:
+                return "Quarta-feira";
+            case 5:
+                return "Quinta-feira";
+            case 6:
+                return "Sexta-feira";
+            case 7:
+                return "Sábado";
+
+            default:
+                return "Dia inválido";
+        }
     }
 }
