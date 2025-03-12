2a9821c5376575d8c4be1aefa7219b6f45542ddb
Maria Antonya Fernandes da Silva <mariaantonyafernandes@gmail.com>
2025-03-12 10:08:54 -0300

    Create Funcoes.java

---
+++ secao6/Funcoes.java
@@ -0,0 +1,15 @@
+package secao6;
+
+public class Funcoes {
+    public static void main(String[] args) {
+        // Criando a primeira função
+        saudacao();
+    }
+
+    /* NIVEL DE ACESSO, STATIC => não preciso instanciar classe para 
+    executar, TIPO DE RETORONO */
+    // NOME, OS PARENTESES (ARGS), BLOCO {}
+    public static void saudacao () {
+        System.out.println("Primeira função!");
+    }
+}
