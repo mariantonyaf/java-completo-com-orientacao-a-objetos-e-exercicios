3631152aa7fb9ee408e96e4e294b63558e7060c1
Maria Antonya Fernandes da Silva <mariaantonyafernandes@gmail.com>
2025-03-12 15:06:10 -0300

    Create FuncaoMath.java

---
+++ secao6/FuncaoMath.java
@@ -0,0 +1,18 @@
+package secao6;
+
+public class FuncaoMath {
+    public static void main(String[] args) {
+        // 1 - Funções de Math
+        System.out.println(Math.sqrt(25));
+
+        System.out.println(Math.pow(2, 3));
+
+        System.out.println(Math.abs(-10));
+        System.out.println(Math.abs(10));
+
+        System.out.println(Math.max(100, 10));
+
+        double raizQuadrada = Math.sqrt(36);
+        System.out.println("A raiz quadrada de 99 é: " + raizQuadrada);
+    }
+}
