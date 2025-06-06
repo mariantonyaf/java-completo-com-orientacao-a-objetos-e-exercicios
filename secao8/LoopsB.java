70152e3e3eaa3ad991b516f5b68d32634e0aed23
Maria Antonya Fernandes da Silva <mariaantonyafernandes@gmail.com>
2025-06-06 08:33:14 -0300

    Create LoopsB.java

---
+++ secao8/LoopsB.java
@@ -0,0 +1,39 @@
+package secao8;
+
+public class LoopsB {
+    public static void main(String[] args) {
+        // 8 - Loops com rótulos
+
+        // Rótulos externos e internos 
+
+        /* externo:
+        for(int i = 0; i < 3; i++) {
+
+            for(int j = 0; j < 3; j++) {
+
+                if(i == 1 && j == 1) {
+                    break externo;
+                }
+
+                System.out.println("i " + i + ", j " + j);
+            }
+        } */
+
+
+        for(int i = 0; i < 3; i++) {
+
+            System.out.println("Externo: " + i);
+
+            interno:
+            for(int j = 0; j < 3; j++) {
+
+                if(j == 2) {
+                    System.out.println("Parou interno");
+                    break interno;
+                }
+
+                System.out.println("i " + i + ", j " + j);
+            }
+        }
+    }
+}
