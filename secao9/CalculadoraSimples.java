aaa1454002ddd5a4a71caa6c881d41463486bf71
Maria Antonya Fernandes da Silva <mariaantonyafernandes@gmail.com>
2025-06-06 09:32:10 -0300

    Update CalculadoraSimples.java

--- secao9/CalculadoraSimples.java
+++ secao9/CalculadoraSimples.java
@@ -1,5 +1,17 @@
 package secao9;
 
+/*
+ * Objetivo: Criar uma calculadora que faz soma, multiplicação, divisão e subtração.
+
+ * 1 - Pedir dois números para o usuário (double).
+ * 2 - Apresentar uma tabela/texto para escolher a operação.
+ * 3 - Resgatar a operação que o usuário selecionou.
+ * 4 - Realizar o calculo.
+ * 5 - Exibir o resultado.
+ * 6 - Se a operação for inválida, exibir mensagem de erro. 
+
+*/
+
 public class CalculadoraSimples {
     
 }
