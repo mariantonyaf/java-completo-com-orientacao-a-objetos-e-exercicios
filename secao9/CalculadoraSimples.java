820e4b710f9a7e652a601f5053eaff8d5648a4e4
Maria Antonya Fernandes da Silva <mariaantonyafernandes@gmail.com>
2025-06-06 09:42:36 -0300

    Update CalculadoraSimples.java

--- secao9/CalculadoraSimples.java
+++ secao9/CalculadoraSimples.java
@@ -5,9 +5,9 @@ import java.util.Scanner;
 /*
  * Objetivo: Criar uma calculadora que faz soma, multiplicação, divisão e subtração.
 
- * 1 - Pedir dois números para o usuário (double).
- * 2 - Apresentar uma tabela/texto para escolher a operação.
- * 3 - Resgatar a operação que o usuário selecionou.
+ * 1 - Pedir dois números para o usuário (double). OK
+ * 2 - Apresentar uma tabela/texto para escolher a operação. OK
+ * 3 - Resgatar a operação que o usuário selecionou. OK
  * 4 - Realizar o calculo.
  * 5 - Exibir o resultado.
  * 6 - Se a operação for inválida, exibir mensagem de erro. 
@@ -31,10 +31,6 @@ public class CalculadoraSimples {
         System.out.println("3 - Multiplicação (*)");
         System.out.println("4 - Divisão (/)");
         int operacao = scanner.nextInt();
-
-        System.out.println(num1);
-        System.out.println(num2);
-        System.out.println(operacao);
 
         scanner.close();
     }
