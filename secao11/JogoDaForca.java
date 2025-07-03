07970d14d6b0ee13971814ee2c8ca5e455987908
Maria Antonya Fernandes da Silva <mariaantonyafernandes@gmail.com>
2025-07-03 07:31:34 -0300

    Update JogoDaForca.java

--- secao11/JogoDaForca.java
+++ secao11/JogoDaForca.java
@@ -31,7 +31,6 @@ package secao11;
     */
 
 import java.util.Scanner;
-import java.util.Arrays;
 
 public class JogoDaForca {
     public static void main(String[] args) {
@@ -39,15 +38,13 @@ public class JogoDaForca {
         Scanner scanner = new Scanner(System.in);
 
         // definir a palavra a ser adivinhada
-        String palavraSecreta = "Java";
+        String palavraSecreta = "JAVA";
 
         char[] palavraOculta = new char[palavraSecreta.length()];
 
         for(int i = 0; i < palavraSecreta.length(); i++) {
             palavraOculta[i] = '_';
         }
-
-        System.out.println(Arrays.toString(palavraOculta));
 
         // número máximo de tentativas
         int tentativas = 6;
@@ -58,6 +55,7 @@ public class JogoDaForca {
             System.out.println("Palavra: " + String.valueOf(palavraOculta));
             System.out.println("Tentativas restantes: " + tentativas);
             System.out.println("Digite uma letra: ");
+
             char letra = scanner.next().toUpperCase().charAt(0);
             System.out.println(letra);
 
@@ -70,9 +68,27 @@ public class JogoDaForca {
                 }
             }
 
-            System.out.println(String.valueOf(palavraOculta));
+            // dedução de tentativas
+            if(!acertou) {
+                tentativas--;
+                System.out.println("Letra incorreta.");
+            } else {
+                System.out.println("Letra correta.");
+            }
 
-            tentativas --;
+            if (String.valueOf(palavraOculta).equals(palavraSecreta)) {
+                venceu = true;
+                break;
+            }
+        }
+
+        // condição de vitória
+        if (venceu) {
+            System.out.println("Parabéns, você venceu! Ainda faltam " + tentativas + " tentativas.");
+            System.out.println("A palavra era: " + palavraSecreta);
+        } else {
+            System.out.println("Você perdeu!");
+            System.out.println("A palavra era: " + palavraSecreta);
         }
 
         scanner.close();
