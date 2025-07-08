7b199329c1b7f19854d9bdde1272d7773a84681f
Maria Antonya Fernandes da Silva <mariaantonyafernandes@gmail.com>
2025-07-08 12:51:14 -0300

    Update POO2.java

--- secao12/POO2.java
+++ secao12/POO2.java
@@ -2,6 +2,8 @@ package secao12;
 
 public class POO2 {
     public static void main(String[] args) {
+
+        // 10 - Modificadores de acesso
         Funcionario funcionario1 = new Funcionario("Maria Antonya", 759, "teste2022");
 
         funcionario1.exibirDados();
